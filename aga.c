6ef4210efb6577b7390cc994c33cfd4cb7daafc3
macieyn <maciej.nachtygal@gmail.com>
2021-05-08 10:28:23 +0200

    zad 90

--- aga.c
+++ aga.c
@@ -1,25 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
+#pragma warning(disable:4996)
+ 
  
 int main() {
-#pragma warning(disable:4996)
-    //-----Zadanie 71-----
-    printf("Zadanie71\n");
-    printf("Program rysuje na ekranie gwiazdki ulozone w trojkat.\n\n");
-    int kolumna, i, j;
-    printf("Podaj ilosc kolumn: ");
-    scanf("%d", &kolumna);
-    for (i = 1; i <= kolumna; i++) {
-        for (j = 1; j < kolumna - i; j++) {
-            printf(" ");
+    //-------Zadanie88-------
+    printf("Zadanie88\n");
+    printf("Program dla zadanej przez uzytkownika tabeli o m wierszach i n kolumnach, wypisuje tabele z zamienionymi wierszami i kolumnami.\n\n");
+    int m, n, i, j, tab[100][100];;
+    srand(time(NULL));
+    printf("Podaj liczbe wierszy: ");
+    scanf("%d", &m);
+    printf("Podaj liczbe kolumn: ");
+    scanf("%d", &n);
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            tab[i][j] = rand() % 20;
         }
-        for (j = 1; j < 2 * i - 1; j++) {
-            printf("*");
+    }
+    printf("\n");
+    printf("Twoja tabela przed zamiana: \n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d\t", tab[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printf("Twoja tabela po zamianie: \n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            printf("%d\t", tab[j][i]);
         }
         printf("\n");
     }
     printf("\n\n");
     printf("Koniec programu.\n");
+ 
+   
     system("pause");
     return 0;
 }
--- zjazd6/zad90/zad90.c
+++ zjazd6/zad90/zad90.c
@@ -2,18 +2,62 @@
 
 int main() {
     int m, n;
+    int array[100][100] = {0};
     printf("Podaj liczbe wierszy: \n");
     scanf("%d", &m);
     printf("Podaj liczbe kolumn: \n");
     scanf("%d", &n);
    
     int number = 0;
+    int sumRow = 0, sumColumn = 0;
+    int badRow = 0, badColumn = 0;
     for (int i=0; i<m; i++) {
         for (int j=0; j<n; j++) {
-           
+            while(1) {
+                printf("Element [%d][%d]: ", i + 1, j + 1);
+                scanf("%d", &number);
+                array[i][j] = number;
+                if (array[i][j] < 0 || array[i][j] > 1) 
+                    printf("Nie poprawna liczba. Wprowadz jeszcze raz.\n");
+                else break;
+            }
         }
         printf("\n");
     }
+
+    printf("Wprowadzona tablica:\n");
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            printf("%2d", array[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            sumRow += array[i][j];
+        }
+        if (sumRow != i + 1) {
+            badRow = 1; 
+            break;
+        }
+    }
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            sumColumn += array[j][i];
+        }
+        if (sumColumn != i + 1) {
+            badColumn = 1; 
+            break;
+        }
+    }
+
+    if (!badColumn) printf("W kazdej kolumnie stoi dokladnie jedna 1.\n");
+    else printf("W ktorejs kolumnie nie stoi dokladnie jedna 1.\n");
+    if (!badRow) printf("W kazdym wierszu stoi dokladnie jedna 1.\n");
+    else printf("W ktoryms wierszu nie stoi dokladnie jedna 1.\n");
+
 
     return 0;
 }
